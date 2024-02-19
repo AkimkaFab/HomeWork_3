349f4b20bb2a1f9629cbc4c8d3ccc01edd67d49c
akimkafab <aafabrikov@mts.ru>
2024-02-20 01:59:39 +0300

    Done RepeatOnFailureExtension

---
+++ src/test/kotlin/MyConfig.kt
@@ -0,0 +1,7 @@
+import io.kotest.core.config.AbstractProjectConfig
+import io.kotest.core.extensions.Extension
+
+
+object MyConfig : AbstractProjectConfig() {
+    override fun extensions(): List<Extension> = listOf(RepeatOnFailureExtension)
+}
--- src/test/kotlin/test.kt
+++ src/test/kotlin/test.kt
@@ -1,10 +1,19 @@
 import data.Priority
 import data.Task
 import data.TasksRepository
+import io.kotest.core.config.AbstractProjectConfig
+import io.kotest.core.extensions.Extension
+import io.kotest.core.extensions.TestCaseExtension
+import io.kotest.core.spec.style.AnnotationSpec
+import io.kotest.core.test.TestCase
+import io.kotest.core.test.TestResult
+import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 import io.qameta.allure.Allure
 
-    class Tests {
+var tryNumber = 1
+
+    class Tests : AnnotationSpec(){
         fun createRepository(): TasksRepository = data.TasksRepositoryMemory()
         val repository = createRepository()
         val taskAmount = (1..10).random()
@@ -19,10 +28,21 @@ import io.qameta.allure.Allure
 
         @Test
         fun createTasksTest() {
+//            println("КОЛИЧЕСТВО ТАСОК: $taskAmount")
+//            if (tryNumber != 1) {
+//                var taskToDelete = 0
+//                do {
+//                    repository.deleteTask(taskToDelete++)
+//                    println("Удалил таску номер $taskToDelete")
+//                } while (taskToDelete != taskAmount)
+//            }
             generateTestData()
             val allTasks = repository.getTasks()
-            assert(allTasks.size == taskAmount) {"ТЕСТ СОЗДАНИЯ ЗАДАЧ НЕ ПРОЙДЕН"}
+            allTasks.size/2 shouldBe taskAmount
+            //assert(allTasks.size == taskAmount) {"ТЕСТ СОЗДАНИЯ ЗАДАЧ НЕ ПРОЙДЕН"}
             println("ТЕСТ СОЗДАНИЯ ЗАДАЧ ПРОЙДЕН")
+
+
         }
 
 
@@ -35,11 +55,13 @@ import io.qameta.allure.Allure
             repository.completeTask(completedTask)
             allTasks.removeAt(completedTask-1)
             val filteredTasks = repository.getTasks(completed = false)
-            assert(filteredTasks == allTasks) {"ТЕСТ ЗАВЕРШЕНИЯ ЗАДАЧИ НЕ ПРОЙДЕН"} // Проверяем, что завершенная задача пропала из фильтра
+            filteredTasks shouldBe allTasks
+            //assert(filteredTasks == allTasks) {"ТЕСТ ЗАВЕРШЕНИЯ ЗАДАЧИ НЕ ПРОЙДЕН"} // Проверяем, что завершенная задача пропала из фильтра
             println("ТЕСТ ЗАВЕРШЕНИЯ ЗАДАЧИ ПРОЙДЕН")
             repository.uncompleteTask(completedTask)
             val allTasks1 = repository.getTasks(completed = false)
-            assert(allTasks1 == allTasksOrigin) {"ТЕСТ ОТМЕНЫ ЗАВЕРШЕНИЯ ЗАДАЧИ НЕ ПРОЙДЕН"} // Проверяем, что после отмены завершения задача вернулась в фильтр и с неё пропал крестик
+            allTasks1 shouldBe allTasksOrigin
+            //assert(allTasks1 == allTasksOrigin) {"ТЕСТ ОТМЕНЫ ЗАВЕРШЕНИЯ ЗАДАЧИ НЕ ПРОЙДЕН"} // Проверяем, что после отмены завершения задача вернулась в фильтр и с неё пропал крестик
             println("ТЕСТ ОТМЕНЫ ЗАВЕРШЕНИЯ ЗАДАЧИ ПРОЙДЕН")
 
         }
@@ -52,8 +74,20 @@ import io.qameta.allure.Allure
             repository.deleteTask(deletedTask)
             allTasks.removeAt(deletedTask-1)
             val allTasks1 = repository.getTasks()
-            assert(allTasks1 == allTasks) {"ТЕСТ УДАЛЕНИЯ ЗАДАЧИ НЕ ПРОЙДЕН"}
+            allTasks1 shouldBe allTasks
+            //assert(allTasks1 == allTasks) {"ТЕСТ УДАЛЕНИЯ ЗАДАЧИ НЕ ПРОЙДЕН"}
             println("ТЕСТ УДАЛЕНИЯ ЗАДАЧИ ПРОЙДЕН")
         }
+
+        @Test
+        fun negativeTaskCounterTest() {
+            println("НЕГАТИВНЫЙ ТЕСТ СЧЁТЧИКА ЗАДАЧ. ПОПЫТКА ${tryNumber++}")
+            generateTestData()
+            val allTasks = repository.getTasks()
+            allTasks.size/2 shouldBe taskAmount+100
+            //assert(allTasks.size == taskAmount+100) {"ТЕСТ СЧЕТЧИКА ЗАДАЧ НЕ ПРОЙДЕН"}
+            println("ТЕСТ СЧЕТЧИКА ЗАДАЧ ПРОЙДЕН")
+        }
+
 
     }
